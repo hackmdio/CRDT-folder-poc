1b929912b3040761163f2ebd53453c5a5f183359
stanley2058 <stanley2058@yahoo.com.tw>
2024-07-29 11:05:06 +0800

    fix: post env collision

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.STATIC_PORT || 3000
+const webrtcPort = process.env.PORT || 4444
+const wsProto = process.env.PROTO || 'ws'
+const host = process.env.HOST || 'localhost'
 
 app.get('/config', (_, res) => {
   res.json({
-    serverUrl: process.env.HOST || 'ws://localhost:4444'
+    serverUrl: `${wsProto}://${host}:${webrtcPort}`
   })
 })
 app.use('/dist', express.static('dist'))
